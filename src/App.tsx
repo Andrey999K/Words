de53003df2569ecdde3fb44f49aa73cd16ac1496
Кутузов Андрей <a.kutuzov@2035.university>
2024-12-24 18:54:50 +0300

    feat: add request for get auth user

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { useGetUser } from "./api/api.ts";
+
 export const App = () => {
+  const { data: userAuth } = useGetUser();
+
+  console.log(userAuth);
+
   return <div className="font-bold text-red-600">12312</div>;
 };
