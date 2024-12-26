b397969bff1d19e4c86e48938de21d6fa51fd64d
Кутузов Андрей <a.kutuzov@2035.university>
2024-12-26 18:15:53 +0300

    feat: add view for home page

--- src/pages/Home/Page.tsx
+++ src/pages/Home/Page.tsx
@@ -1,5 +1,23 @@
-import { Button } from "antd";
+import { Button, Radio, RadioChangeEvent } from "antd";
+import { useState } from "react";
 
 export const Home = () => {
-  return <div className="flex flex-col gap-4"><Button>Начать игру</Button></div>;
+  const [value, setValue] = useState(1);
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
+  return <div className="w-full h-screen flex items-center justify-center">
+    <div className="flex flex-col gap-4">
+      <Radio.Group onChange={onChange} value={value} className="flex flex-col gap-3">
+        <Radio value={1}>Обучение</Radio>
+        <Radio value={2}>Лёгкий</Radio>
+        <Radio value={3}>Средний</Radio>
+        <Radio value={4}>Сложный</Radio>
+      </Radio.Group>
+      <Button>Начать игру</Button>
+    </div>
+  </div>;
 };
---
+++ src/pages/Home/StartFrame.tsx
@@ -0,0 +1,7 @@
+export const StartFrame = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
