ce6c49f015527f64440d7fe7309a3dcb86e5b913
Кутузов Андрей <a.kutuzov@2035.university>
2024-12-27 01:19:47 +0300

    feat: add mock data

--- src/pages/Home/GameFrame.tsx
+++ src/pages/Home/GameFrame.tsx
@@ -1,17 +1,22 @@
 import { Card, Input } from "antd";
+import { mockData } from "./mockData.ts";
 
 export const GameFrame = () => {
   return (
     <div className="h-screen pt-40 w-full flex flex-col items-center">
       <div className="w-full max-w-[60ch]">
         <Input className="w-full" placeholder="Введите слово" />
-        <div className="flex flex-col mt-5 w-full">
-          <Card className="font-bold border-gray-500 border-2">
-            <div className="flex justify-between w-full items-center">
-              <span>Слово</span>
-              <span>234</span>
-            </div>
-          </Card>
+        <div className="flex flex-col mt-5 w-full gap-2">
+          {
+            mockData.map(word => (
+              <Card key={word.id} className="font-bold border-gray-500 border-2">
+                <div className="flex justify-between w-full items-center">
+                  <span>{word.word}</span>
+                  <span>{word.guess}</span>
+                </div>
+              </Card>
+            ))
+          }
         </div>
       </div>
     </div>
