b60c6890bbb1771afa3a2dab64ff55977468e5fd
Кутузов Андрей <a.kutuzov@2035.university>
2025-03-02 15:39:45 +0300

    fix: close win modal after click cross

--- src/pages/Home/WinModal.tsx
+++ src/pages/Home/WinModal.tsx
@@ -12,8 +12,14 @@ export const WinModal: FC<WinModalProps> = ({ data, onOk }) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <Modal title="Победа!" open={!!data} onOk={onOk} okText="Новая игра" cancelButtonProps={{ hidden: true }}
-           className={theme?.darkTheme ? "dark-theme !shadow-none" : ""}
+    <Modal
+      title="Победа!"
+      open={!!data}
+      onOk={onOk}
+      onCancel={onOk}
+      okText="Новая игра"
+      cancelButtonProps={{ hidden: true }}
+      className={theme?.darkTheme ? "dark-theme !shadow-none" : ""}
     >
       <p>Ты угадал, йоу, красава, мээээээээээээээээээээээээээээээн!!!!!!!🤪🤪🤪</p>
       <p>Загаданное слово <b>{data.guess}</b>.</p>
