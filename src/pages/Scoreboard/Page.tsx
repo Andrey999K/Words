d8e87a3da21ae23efcde413b3078fadd8f706338
Кутузов Андрей <a.kutuzov@2035.university>
2025-03-22 22:21:45 +0300

    feat: add cups for scoreboard

--- src/pages/Scoreboard/Page.tsx
+++ src/pages/Scoreboard/Page.tsx
@@ -3,10 +3,31 @@ import { Card } from "antd";
 import { PageLoader } from "../../components/PageLoader.tsx";
 import { usePageTitle } from "../../hooks/usePageTitle.ts";
 import { Link } from "react-router-dom";
+import bronzeCup from "../../assets/cup/bronze.png";
+import silverCup from "../../assets/cup/silver.png";
+import goldCup from "../../assets/cup/gold.png";
 
 export const Scoreboard = () => {
   const { data: scoreboard, isLoading } = useGetScoreboard();
   usePageTitle("Топ игроков");
+
+  const renderCup = (cupType: "gold" | "silver" | "bronze" | "") => {
+    let cup = "";
+    switch (cupType) {
+      case "bronze":
+        cup = bronzeCup;
+        break;
+      case "silver":
+        cup = silverCup;
+        break;
+      case "gold":
+        cup = goldCup;
+        break;
+      default:
+        return;
+    }
+    return <img src={cup} alt="" />;
+  };
 
   if (isLoading) return <PageLoader />;
 
@@ -20,6 +41,11 @@ export const Scoreboard = () => {
               <Card className="p-4 w-full">
                 <div className="flex justify-between items-center gap-5">
                   <div className="flex gap-2 items-center">
+                    <div className="w-8">
+                      {index === 0 && renderCup("gold")}
+                      {index === 1 && renderCup("silver")}
+                      {index === 2 && renderCup("bronze")}
+                    </div>
                     <b>{index + 1}.</b>
                     <span>{user.email}</span>
                   </div>
